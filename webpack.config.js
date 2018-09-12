9adda17de3c634279673ebbeb1862d78e21e2560
Jani Luukko <jani.luukko@reaktor.com>
2018-09-12 15:33:12 +0300

    update deps

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
       exclude: /node_modules/,
       loader: 'babel',
       query: {
-        presets: ['react', 'es2015', 'stage-1']
+        presets: ['react', 'env', 'stage-1']
       }
     }]
   },
