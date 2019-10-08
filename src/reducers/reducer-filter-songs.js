bd2aef90602e7996597b7eb173e04eda92cf94c5
Juha Tauriainen <nakkeja@gmail.com>
2019-10-08 15:33:39 +0300

    🌽
    Simplify (?)

--- src/reducers/reducer-filter-songs.js
+++ src/reducers/reducer-filter-songs.js
@@ -1,5 +1,7 @@
-export default function(state = null, action) {
-    return Object.assign({}, state, {
-        filteredSongs: state.songs.filter((song) => {return song.title.toLowerCase().indexOf(action.payload.toLowerCase()) != -1})
-    })
-}
+export default (state = null, action) => ({
+  ...state,
+  filteredSongs: state.songs.filter(
+    ({ song }) =>
+      title.toLowerCase().indexOf(action.payload.toLowerCase()) != -1,
+  ),
+})
