628bf72a22560edbdd965fa1f5af08b86dbc2361
Jani Luukko <jani.luukko@reaktor.com>
2022-03-26 16:53:56 +0200

    clarify song interface fields

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,12 @@
 export interface Song {
+  /** ID of the song */
   id: number;
+  /** Name of the song */
   name: string;
+  /** Melody of the song */
   melody: string;
+  /** Writer of the song */
   lyricsBy: string;
+  /** Lyrics of the song */
   lyrics: string;
 }
