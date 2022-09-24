97efdf2504a5fdedb63c44f04b799cfd58d991e0
Jani Luukko <jani.luukko@reaktor.com>
2022-09-24 12:48:12 +0300

    Update deps & remove GA code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Route, Routes, BrowserRouter, Navigate } from "react-router-dom";
-import { createBrowserHistory, Update } from "history";
-import ReactGA from "react-ga";
+import { createBrowserHistory } from "@remix-run/router";
 
 import songsJson from "./songs.json";
 import "./App.css";
@@ -13,31 +12,8 @@ const songs = songsJson as Song[];
 
 const history = createBrowserHistory();
 
-const GA_CODE = process.env.REACT_APP_GA_CODE;
-
-if (GA_CODE) {
-  ReactGA.initialize(GA_CODE, {});
-}
-
-const isUpdate = (presumed: any): presumed is Update =>
-  presumed.location !== undefined;
-
-const trackGa = (event: Location | Update) => {
-  // Update is an object { action: Action, location: Location }
-  const location = isUpdate(event) ? event.location : event;
-  const completePath = location.pathname + location.search;
-
-  if (GA_CODE) {
-    ReactGA.pageview(completePath);
-  }
-};
-
-// Send initial page load as pageview
-trackGa(window.location);
-
 history.listen((location) => {
   window.scrollTo(0, 0);
-  if (GA_CODE) trackGa(location);
 });
 
 function App() {
