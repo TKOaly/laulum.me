fcde05a5f9e734c6fd51cba2007ce28f940986bf
Eetu Raunio <eetu.raunio@helsinki.fi>
2022-10-28 21:29:16 +0300

    some minor linting changes

--- src/SongList.tsx
+++ src/SongList.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 import { extract, partial_ratio } from "fuzzball";
 import { Link, useNavigate } from "react-router-dom";
 import { Helmet } from "react-helmet";
