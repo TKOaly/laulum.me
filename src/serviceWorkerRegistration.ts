8d40ac388413f542647d999c933cdde4beadc591
Eetu Raunio <eetu.raunio@helsinki.fi>
2022-10-28 17:44:15 +0300

    add info message for sw not registered due to cdn

--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -39,6 +39,9 @@ export function register(config?: Config) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
       // serve assets; see https://github.com/facebook/create-react-app/issues/2374
+      console.info(
+        "Service worker not registered as PUBLIC_URL origin differs from current location"
+      );
       return;
     }
 
