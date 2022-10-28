33900cf42eb2413262e1da4aa5994fe94cacdaee
Eetu Raunio <eetu.raunio@helsinki.fi>
2022-10-28 17:15:22 +0300

    enable service worker for offline functionality
    closes #50

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import * as serviceWorker from "./serviceWorkerRegistration";
 import "typeface-roboto";
 
 createRoot(document.getElementById("root")!).render(
@@ -13,4 +13,4 @@ createRoot(document.getElementById("root")!).render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
