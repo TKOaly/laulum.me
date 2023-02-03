9a8a6dcd130378f5e19d20dda8f32bf004615d7c
Eetu Raunio <eetu.raunio@helsinki.fi>
2023-02-04 00:20:38 +0200

    add fuzzy sort

---
+++ components/Input.tsx
@@ -0,0 +1,6 @@
+import { HTMLProps } from "react";
+import styles from "./Input.module.css";
+
+export const Input = (props: HTMLProps<HTMLInputElement>) => (
+  <input {...props} className={styles.input} />
+);
