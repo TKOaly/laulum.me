9c9a2f56ddcacf6c016cc412446c1fc854bd0f47
Eetu Raunio <eetu.raunio@helsinki.fi>
2023-02-05 23:24:25 +0200

    use slugify for titles

--- lib/songs.ts
+++ lib/songs.ts
@@ -3,11 +3,56 @@ import type { Song } from "@/types/song";
 import fm from "front-matter";
 import { promises } from "fs";
 import path from "path";
+import slugify from "./slugify";
 
 const directory = path.join(process.cwd(), "songs");
+const cache = path.join(process.cwd(), "cache", "songs.json");
 
 const getFilenames = async () => {
   return promises.readdir(directory);
+};
+
+// Since slugs do not match filenames, we need a mapping between the two.
+const getSlugs = async (
+  querySlug?: string
+): Promise<Array<{ slug: string; filename: string }>> => {
+  try {
+    const cachedData = await promises.readFile(cache, "utf-8");
+    const mapping = JSON.parse(cachedData);
+
+    // If we cannot find the specified querySlug in the cache data, we might need to update our cache.
+    // Mainly useful in dev environments.
+    if (!mapping.some(({ slug }: { slug: string }) => slug === querySlug)) {
+      throw Error(
+        `Potential cache miss for querySlug ${querySlug}, revalidating...`
+      );
+    }
+
+    return mapping;
+  } catch (e) {}
+
+  const filenames = await getFilenames();
+
+  const songs = await Promise.all(
+    filenames.map(async (filename) => {
+      const song = await readSong(filename);
+      return { ...song, filename };
+    })
+  );
+
+  const mapping = songs.map(({ slug, filename }) => ({ slug, filename }));
+
+  try {
+    await promises.mkdir(path.join(process.cwd(), "cache"));
+  } catch (e) {}
+
+  try {
+    await promises.writeFile(cache, JSON.stringify(mapping), "utf-8");
+  } catch (e) {
+    console.error(e);
+  }
+
+  return mapping;
 };
 
 const readSong = async (filename: string) => {
@@ -15,26 +60,25 @@ const readSong = async (filename: string) => {
   const fileContents = await promises.readFile(filePath, "utf-8");
 
   const content = fm<Song>(fileContents);
+  const { title } = content.attributes;
   const lyrics = content.body;
 
-  return { ...content.attributes, lyrics, slug: path.parse(filename).name };
+  return { ...content.attributes, lyrics, slug: slugify(title) };
 };
 
 export const getSongs = async () => {
   const filenames = await getFilenames();
-
   const songs = Promise.all(filenames.map(readSong));
-
   return songs;
 };
 
-export const getSong = async (slug: string): Promise<Song | null> => {
-  const filenames = await getFilenames();
-  const filename = `${slug}.md`;
+export const getSong = async (querySlug: string): Promise<Song | null> => {
+  const mapping = await getSlugs(querySlug);
+  const match = mapping.find(({ slug }) => querySlug === slug);
 
-  if (!filenames.includes(filename)) {
+  if (!match) {
     return null;
   }
 
-  return readSong(filename);
+  return readSong(match.filename);
 };
