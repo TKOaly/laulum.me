96551d40e5745d78ec87daa07e33eeeb4914d43b
Eetu Raunio <eetu.raunio@helsinki.fi>
2023-02-22 22:39:15 +0200

    fix dynamic back button

--- components/BackButton.tsx
+++ components/BackButton.tsx
@@ -1,13 +1,33 @@
+import useMounted from "@/lib/useMounted";
 import { useRouter } from "next/router";
 import { useCallback } from "react";
 import { Link } from "./Link";
 
+import styles from "./Link.module.css";
+
 export const BackButton = () => {
+  const mounted = useMounted();
   const router = useRouter();
   const goBack = useCallback(() => router.back(), [router]);
+
+  // Show normal link until mounted
+  if (!mounted) {
+    return (
+      <Link variant="primary" href="/">
+        Back
+      </Link>
+    );
+  }
+
+  // When mounted, display button to go back.
+  // This is not accessible!
   return (
-    <Link variant="primary" href="/" onClick={goBack}>
+    <button
+      onClick={goBack}
+      className={`${styles.link} ${styles.primary}`}
+      style={{ cursor: "pointer" }}
+    >
       Back
-    </Link>
+    </button>
   );
 };
