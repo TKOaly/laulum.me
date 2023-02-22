dc1f653ca1d58df36ebc1b71a31275a1c935f86e
Eetu Raunio <eetu.raunio@helsinki.fi>
2023-02-22 19:16:28 +0200

    scroll to input

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,6 +1,7 @@
-import type { HTMLProps } from "react";
+import { DetailedHTMLProps, forwardRef, InputHTMLAttributes } from "react";
 import styles from "./Input.module.css";
 
-export const Input = (props: HTMLProps<HTMLInputElement>) => (
-  <input {...props} className={styles.input} />
-);
+export const Input = forwardRef<
+  HTMLInputElement,
+  DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+>((props, ref) => <input ref={ref} {...props} className={styles.input} />);
