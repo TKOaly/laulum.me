eadf4545a32b3ddab60be4287d9841c1ebec3885
Eetu Raunio <me@concernedhobbit.eu>
2023-03-09 19:03:53 +0200

    revert native functions in tg url composition
    this caused pluses in song text

--- lib/getTelegramUrl.ts
+++ lib/getTelegramUrl.ts
@@ -6,12 +6,10 @@ const BYTE_LIMIT = 4096;
 const TELEGRAM_BASE = "https://t.me/share/url";
 const FALLBACK_BASE_URL = "https://laulum.me/";
 
-export const getTelegramURL = (songUrl: string, text?: string): string => {
-  const url = new URL(TELEGRAM_BASE);
-  url.searchParams.set("url", songUrl);
-  if (text) url.searchParams.set("text", text);
-  return url.toString();
-};
+const getTelegramURL = (url: string, text: string): string =>
+   `${TELEGRAM_BASE}?url=${encodeURIComponent(url)}&text=${encodeURIComponent(
+     text
+   )}`;
 
 /**
  * A function to generate a Telegram share link for a song.
